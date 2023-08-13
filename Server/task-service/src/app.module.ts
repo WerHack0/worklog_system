1eec92dca7565a9840cf0725dd05c0c61543b584
WerHack0 <109349061+WerHack0@users.noreply.github.com>
2023-08-13 04:54:56 +0200

    Update app.module.ts

--- Server/task-service/src/app.module.ts
+++ Server/task-service/src/app.module.ts
@@ -15,6 +15,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DATABASE,
       entities: [Log],
+    
       synchronize:true,
     }),
     TypeOrmModule.forFeature([Log]),
